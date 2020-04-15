41827fbc1a6675fd1a8fff75630d5405d7343e82
pennShoe <61952882+pennShoe@users.noreply.github.com>
2020-04-15 13:35:18 -0400

    Update StockGrapher.java

--- backup/StockGrapher.java
+++ backup/StockGrapher.java
@@ -1,11 +1,19 @@
 import java.io.*;
+import java.util.*;
 
 
 public class StockGrapher {
 	
 	public static void main(String[] args) throws IOException{
 		StockDataReader stock = new StockDataReader("IBM", 5);
-		stock.fetchData();
+		HashMap<Integer, StockData> stockData = stock.fetchData();
+		
+		DataConverter converter = new DataConverter(stockData);
+		String[] times = converter.getStockTimes();
+		double[] values = converter.getStockValues();
+		
+		GraphDrawer graph = new GraphDrawer(times, values);
+		graph.drawGraph();
 	}
 
 }
