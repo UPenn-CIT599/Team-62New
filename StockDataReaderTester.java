9d9afd81507cd22b29b0d772218c1038b22e81cb
pennShoe <61952882+pennShoe@users.noreply.github.com>
2020-04-24 16:59:28 -0400

    Update StockDataReaderTester.java
    updated to include start date check

--- StockDataReaderTester.java
+++ StockDataReaderTester.java
@@ -1,32 +1,55 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.HashMap;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+
+
 /**
- * Validation tests for StockDataReader
+ * Validation test for StockDataReader
  * 
  * @author jeris
  *
  */
+
 public class StockDataReaderTester {
 	
 	static int testDays;
 	static HashMap<Integer, StockData> testMap = new HashMap<Integer, StockData>();
+	static LocalDate date;
+	static LocalDate stockDate;
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws IOException {
 		testDays = 20;
 		StockDataReaderModified stock = new StockDataReaderModified("IBM", testDays);
 		testMap = stock.fetchData();
+		StockData stockData = testMap.get(0);
+		stockDate = LocalDate.parse(stockData.getTime());
+		date = LocalDate.now();
+		String checkWeekend = date.toString();
+		if(checkWeekend.equals("SUNDAY")) {
+			date = date.minusDays(2);
+		}
+		if(checkWeekend.equals("SATURDAY")) {
+			date = date.minusDays(1);
+		}
+		
 
 	}
 	
+	//validate that the number of days being returned for 1 month is 20 days or greater
 	@Test
 	public void numberOfDaysTest() {
 		assertTrue(testMap.size() >= testDays);
 	}
 	
+	//validate that the first date being returned is today's date
+	public void dateTest() {
+		assertEquals(date, stockDate);
+	}
 
+	
 }
