2a9c39190ce041a9fbdc308be8f6d4e4e6f519ea
pennShoe <61952882+pennShoe@users.noreply.github.com>
2020-04-24 17:53:59 -0400

    Update DataConverterTester.java
    adding two new unit tests.

--- DataConverterTester.java
+++ DataConverterTester.java
@@ -10,21 +10,36 @@ class DataConverterTester {
 	static HashMap<Integer, StockData> testMap = new HashMap<Integer, StockData>();
 	static int times;
 	static int values;
+	static int map;
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws IOException {
 		StockDataReaderModified stock = new StockDataReaderModified("IBM", 20);
-		converter = new DataConverter(stock.fetchData());
+		testMap = stock.fetchData();
+		map = testMap.size();
+		converter = new DataConverter(testMap);
 		times = converter.getStockTimes().length;
 		values = converter.getStockValues().length;
 
 	}
 	
-
+	//confirm that the converted arrays are the same size
 	@Test
-	void test() {
+	void TimesValuesSizeTest() {
 		
 		assertEquals(times, values); 
+	}
+	
+	//confirm that the converted time data is the same size as the hashMap
+	@Test
+	void convertedTimesSizeTest() {
+		assertEquals(map, times);
+	}
+	
+	//confirm that the converted price data is the same size as the hashMap
+	@Test
+	void convertedValuesSizeTest() {
+		assertEquals(map, values);
 	}
 
 }
