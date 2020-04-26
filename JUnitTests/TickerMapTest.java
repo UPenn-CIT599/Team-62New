f5667d3d9fc7cbd4bc821d14ce8f4e859332179e
graceparkseas <59761449+graceparkseas@users.noreply.github.com>
2020-04-26 18:24:09 -0400

    Updated comments of test

--- JUnitTests/TickerMapTest.java
+++ JUnitTests/TickerMapTest.java
@@ -7,7 +7,8 @@ import org.junit.jupiter.params.provider.CsvSource;
 class TickerMapTest {
     TickerMap map = new TickerMap();
 
-
+    
+    //Tests that the getTickerArray() method returns all 46 ticker symbols within the tickers TreeMap 
     @Test
     void testGetTickerArray() {  
         String[] expectedArr = {"AAPL", "AMZN", "AXP", "BA", "BAC", "BRK.B", "CAT", "CMCSA", "CSCO", "CVX", 
@@ -19,7 +20,7 @@ class TickerMapTest {
     }
         
     
-
+    //Tests that the getNameOfTicker() method returns the appropriate company name when a ticker symbol is given
     @ParameterizedTest
     @CsvSource({
         "'HD', 'Home Depot'",
